59f8ddd18714528c330cce5d64c483cf0207626f
unknown <ksh931101@naver.com>
2024-01-23 13:36:10 +0900

    0123

--- js/tetris.js
+++ js/tetris.js
@@ -1,12 +1,136 @@
+// DOM
+
 const playground = document.querySelector(".playground > ul");
 
-console.log(playground);
+// SETTING
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
 
-for (let i = 0; i < 20; i++) {
+// variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+  tree: [
+    [
+      [2, 1],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+    [
+      [1, 2],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+    [
+      [1, 2],
+      [0, 1],
+      [2, 1],
+      [1, 1],
+    ],
+    [
+      [2, 1],
+      [1, 2],
+      [1, 0],
+      [1, 1],
+    ],
+  ],
+};
+
+const movingItem = {
+  type: "tree",
+  direction: 3,
+  top: 0,
+  left: 3,
+};
+
+init();
+
+// functions
+function init() {
+  tempMovingItem = { ...movingItem };
+  for (let i = 0; i < GAME_ROWS; i++) {
+    prependNewLine();
+  }
+  renderBlocks();
+}
+
+function prependNewLine() {
   const li = document.createElement("li");
   const ul = document.createElement("ul");
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < GAME_COLS; j++) {
     const matrix = document.createElement("li");
     ul.prepend(matrix);
   }
+  li.prepend(ul);
+  playground.prepend(li);
 }
+
+function renderBlocks() {
+  const { type, direction, top, left } = tempMovingItem;
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove(type, "moving");
+    console.log(moving);
+  });
+  BLOCKS[type][direction].forEach((block) => {
+    const x = block[0] + left;
+    const y = block[1] + top;
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
+      : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, "moving");
+    } else {
+      tempMovingItem = { ...movingItem };
+      setTimeout(() => {
+        renderBlocks();
+        if (moveType === "top") {
+          seizeBlock();
+        }
+      }, 0);
+    }
+  });
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
+}
+
+function seizeBlock() {
+  console.log("seize block");
+}
+
+function checkEmpty(target) {
+  if (!target) {
+    return false;
+  }
+  return true;
+}
+
+function moveBlock(moveType, amount) {
+  tempMovingItem[moveType] += amount;
+  renderBlocks();
+}
+
+//event handling
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 39:
+      moveBlock("left", 1);
+      break;
+    case 37:
+      moveBlock("left", -1);
+      break;
+    case 40:
+      moveBlock("top", 1);
+      break;
+    default:
+      break;
+  }
+  // console.log(e);
+});
